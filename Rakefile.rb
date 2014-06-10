d7b5dcab362aca166cce57b6e4baea8ea4700a69
Alexey Shumkin <Alex.Crezoff@gmail.com>
2014-06-10 17:31:30 +0400

    improve: tests: use explicitly defined DELPHI_VERSION env var to run tests

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,8 +5,14 @@ require 'rake/testtask'
 
 task :default => :test
 
+task :"test:prerequisites" do
+    raise 'Please define DELPHI_VERSION environment variable' \
+		+ ' to run tests with appropriate Delphi compiler' unless ENV['DELPHI_VERSION']
+end
+
 Rake::TestTask.new('test') do |t|
     t.ruby_opts << '-d' if Rake.application.options.trace
     t.libs << 'test'
     t.verbose = true
+    Rake::application[t.name].enhance([:"test:prerequisites"])
 end
