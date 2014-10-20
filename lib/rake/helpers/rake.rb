2b775fee7b47c6f67b67dcb380cbfb3c797d1524
Alexey Shumkin <Alex.Crezoff@gmail.com>
2014-10-20 12:55:56 +0400

    improved: added Rake.set_env_to_vars method
    This added method takes environment variables by a given list and adds
    them to a variables list.

--- lib/rake/helpers/rake.rb
+++ lib/rake/helpers/rake.rb
@@ -16,6 +16,27 @@ module Rake
         end
     end
 
+    def self.set_env_to_vars(vars_list, vars, envVarsEncoding, localEncoding)
+        vars_list.flatten.each do |v|
+            value = ENV[v]
+            if value
+                value = value.dup
+                # Env vars are in system encoding
+                # On Windows/Cygwin only
+                if value.respond_to?(:force_encoding)
+                    if Rake.application.windows? || Rake.cygwin?
+                        value.force_encoding(envVarsEncoding)
+                        value.encode!(localEncoding, envVarsEncoding)
+                    end
+                elsif Rake.application.windows?
+                    require 'iconv'
+                    value = Iconv.iconv(localEncoding, envVarsEncoding, value)[0]
+                end
+            end
+            vars[v.to_sym] = value
+        end
+    end
+
     def self.quotepath(switch, path)
         return ! path.to_s.empty? ? "#{switch}\"#{path.to_s}\"" : ''
     end
