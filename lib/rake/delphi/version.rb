fe488d094fc6201716ed607165648921bb3bf502
Alexey Shumkin <Alex.Crezoff@gmail.com>
2015-01-21 13:20:42 +0300

    fixed: lookaheads do not work for patterns in `.rake.ignored.libs` files

--- lib/rake/delphi/version.rb
+++ lib/rake/delphi/version.rb
@@ -2,6 +2,6 @@
 
 module Rake
   module Delphi
-    VERSION = '0.0.23'
+    VERSION = '0.0.24'
   end
 end
--- lib/rake/helpers/filelist.rb
+++ lib/rake/helpers/filelist.rb
@@ -4,7 +4,13 @@ require 'rake'
 module Rake
     class FileList
         def self.get_ignored_dir_pattern(dir)
-            Regexp.new("(^|[\\/\\\\])#{dir}([\\/\\\\]|$)", true)
+            # if a pattern contains lookahead
+            # # do not add trailing part
+            if /\(\?[=!]/ =~ dir
+                return Regexp.new("(^|[\\/\\\\])#{dir}", true)
+            else
+                return Regexp.new("(^|[\\/\\\\])#{dir}([\\/\\\\]|$)", true)
+            end
         end
         IGNORE_GIT_PATTERN = get_ignored_dir_pattern('.git')
         DEFAULT_IGNORE_PATTERNS << IGNORE_GIT_PATTERN \
